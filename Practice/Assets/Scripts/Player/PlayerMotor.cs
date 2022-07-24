f430ff01aa45634d5fca88d00fa9c90cb7303b5a
John Wilson <ashuresyt@gmail.com>
2022-07-25 00:49:42 +1200

    Added player jump + jump counter

--- Practice/Assets/Scripts/Player/PlayerMotor.cs
+++ Practice/Assets/Scripts/Player/PlayerMotor.cs
@@ -7,9 +7,10 @@ public class PlayerMotor : MonoBehaviour
     private Rigidbody2D rb;
 
     public float speed;
-    public float jumpHeight;
+    public float jumpForce;
 
     private float moveDirection;
+    private int jumps;
 
     
     void Awake()
@@ -22,5 +23,15 @@ public class PlayerMotor : MonoBehaviour
     {
         moveDirection = Input.GetAxis("Horizontal");
         transform.Translate(moveDirection * speed * Time.deltaTime, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.W) && jumps < 2)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumps += 1;
+        }
+        if (Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            jumps = 0;
+        }
     }
 }
