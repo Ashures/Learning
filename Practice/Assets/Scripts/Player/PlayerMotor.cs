9ff16d50ca587c7a9fb9f3c0a9f0503ee1f8e0c3
John Wilson <ashuresyt@gmail.com>
2022-07-25 02:29:44 +1200

    Changed camera follow + fixed jitter movement

--- Practice/Assets/Scripts/Player/PlayerMotor.cs
+++ Practice/Assets/Scripts/Player/PlayerMotor.cs
@@ -7,9 +7,10 @@ public class PlayerMotor : MonoBehaviour
     private Rigidbody2D rb;
 
     public float speed;
-    public float jumpForce;
+    public float jumpForce = 7;
 
     private float moveDirection;
+    private bool isMoving;
     private int jumps;
 
     
@@ -22,10 +23,17 @@ public class PlayerMotor : MonoBehaviour
     void Update()
     {
         moveDirection = Input.GetAxis("Horizontal");
-        transform.Translate(moveDirection * speed * Time.deltaTime, 0, 0);
-
-        if(Input.GetKeyDown(KeyCode.W) && jumps < 2)
+        isMoving = (moveDirection != 0);
+        if (isMoving)
         {
+            //transform.Translate(moveDirection * speed * Time.deltaTime, 0, 0);
+            rb.MovePosition(new Vector2((transform.position.x + moveDirection * speed * Time.deltaTime), transform.position.y));
+        }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.W) && jumps < 2)
+        {
+            rb.velocity = Vector3.zero;
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             jumps += 1;
         }
